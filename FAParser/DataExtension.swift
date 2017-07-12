495bc53481e575aeec71866a6e5363db8991b52b
Muhammad Afroz <muhammadafroz@miss-mac-mini-6.local>
2017-07-12 13:09:22 +0500

    - Update dictionary extension

--- FAParser/DataExtension.swift
+++ FAParser/DataExtension.swift
@@ -20,26 +20,14 @@ extension Dictionary {
         dict.merge(with: dictionary)
         return dict
     }
-    
-    /// SwifterSwift: Check if key exists in dictionary.
-    ///
-    /// - Parameter key: key to search for
-    /// - Returns: true if key exists in dictionary.
     public func has(key: Key) -> Bool {
         return index(forKey: key) != nil
     }
     
-    /// SwifterSwift: Remove all keys of the dictionary.
-    ///
-    /// - Parameter keys: keys to be removed
     public mutating func removeAll(keys: [Key]) {
         keys.forEach({ removeValue(forKey: $0)})
     }
     
-    /// SwifterSwift: JSON Data from dictionary.
-    ///
-    /// - Parameter prettify: set true to prettify data (default is false).
-    /// - Returns: optional JSON Data (if applicable).
     public func jsonData(prettify: Bool = false) -> Data? {
         guard JSONSerialization.isValidJSONObject(self) else {
             return nil
@@ -47,11 +35,6 @@ extension Dictionary {
         let options = (prettify == true) ? JSONSerialization.WritingOptions.prettyPrinted : JSONSerialization.WritingOptions()
         return try? JSONSerialization.data(withJSONObject: self, options: options)
     }
-    
-    /// SwifterSwift: JSON String from dictionary.
-    ///
-    /// - Parameter prettify: set true to prettify string (default is false).
-    /// - Returns: optional JSON String (if applicable).
     public func jsonString(prettify: Bool = false) -> String? {
         guard JSONSerialization.isValidJSONObject(self) else {
             return nil
@@ -65,10 +48,6 @@ extension Dictionary {
 // MARK: - Methods
 public extension Data {
     
-    /// SwifterSwift: String by encoding Data using the given encoding (if applicable).
-    ///
-    /// - Parameter encoding: encoding.
-    /// - Returns: String by encoding Data using the given encoding (if applicable).
     public func string(encoding: String.Encoding) -> String? {
         return String(data: self, encoding: encoding)
     }
